1abc12c017055d3df6e299b38bdce6618adf166c
Dev SHRESTHA <mrdev04stha@gmail.com>
2026-01-20 10:35:01 +0545

    fix: Update loading image source to use external URL

--- app/components/loading.jsx
+++ app/components/loading.jsx
@@ -5,7 +5,7 @@ export default function Loading() {
   return (
     <div className="fixed inset-0 bg-white z-50 flex items-center justify-center">
       <Image
-        src="/loading.gif"
+        src="https://i.ibb.co/NgdjtmLH/loading.gif"
         alt="Loading..."
         width={150}  
         height={150} 
